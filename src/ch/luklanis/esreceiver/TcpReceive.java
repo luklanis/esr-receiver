0b3c255e83a5bc6f45de6e5d02bda13632ce7281
Lukas Landis <landis.lukas@gmail.com>
2013-06-06 15:03:46 +0200

    filter keep alives

--- src/ch/luklanis/esreceiver/TcpReceive.java
+++ src/ch/luklanis/esreceiver/TcpReceive.java
@@ -7,20 +7,26 @@ import java.net.Socket;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import ch.luklanis.esreceiver.connectionstate.ConnectionState;
 import ch.luklanis.esreceiver.connectionstate.ConnectionStateChangedEvent;
 import ch.luklanis.esreceiver.connectionstate.OnConnectionStateChangeListener;
 import ch.luklanis.esreceiver.datareceived.DataReceivedEvent;
 import ch.luklanis.esreceiver.datareceived.OnDataReceivedListener;
 
-public class TcpReceive  implements Runnable{
+public class TcpReceive implements Runnable {
 
 	// Declaration section
 	// clientClient: the client socket
 	// os: the output stream
 	// is: the input stream
 	private static final AtomicBoolean close = new AtomicBoolean(false);
-	private static final AtomicInteger currentState = new AtomicInteger(ConnectionState.Disconnected.ordinal());
+	private static final AtomicInteger currentState = new AtomicInteger(
+			ConnectionState.Disconnected.ordinal());
+	private static final String KEEP_ALIVE = "KA";
+
+	private static final String STOP_CONNECTION = "STOP";
 
 	static Socket clientSocket = null;
 	static DataInputStream is = null;
@@ -33,14 +39,15 @@ public class TcpReceive  implements Runnable{
 	private static int port;
 	private Thread thread;
 
-	public void setOnConnectionStateChangeListener(OnConnectionStateChangeListener listener) {
+	public void setOnConnectionStateChangeListener(
+			OnConnectionStateChangeListener listener) {
 		onConnectionStateChangeListener = listener;
 	}
 
 	public void setOnDataReceivedListener(OnDataReceivedListener listener) {
 		onDataReceivedListener = listener;
 	}
-	
+
 	public void close() {
 		close.set(true);
 		thread.interrupt();
@@ -53,7 +60,7 @@ public class TcpReceive  implements Runnable{
 		close.set(false);
 		TcpReceive.host = host;
 		TcpReceive.port = port;
-		
+
 		changeConnectionState(ConnectionState.Connecting);
 
 		this.thread = new Thread(new TcpReceive());
@@ -63,27 +70,28 @@ public class TcpReceive  implements Runnable{
 	protected void changeConnectionState(ConnectionState state) {
 		currentState.set(state.ordinal());
 		if (onConnectionStateChangeListener != null) {
-			onConnectionStateChangeListener.connectionStateChanged(
-					new ConnectionStateChangedEvent(this, state));
+			onConnectionStateChangeListener
+					.connectionStateChanged(new ConnectionStateChangedEvent(
+							this, state));
 		}
-	}     
+	}
 
 	protected void dataReceived(String responseLine) {
 		if (onDataReceivedListener != null) {
-			onDataReceivedListener.dataReceived(
-					new DataReceivedEvent(this, responseLine));
+			onDataReceivedListener.dataReceived(new DataReceivedEvent(this,
+					responseLine));
 		}
-	}  
-	
+	}
+
 	public ConnectionState getCurrentState() {
 		return ConnectionState.values()[currentState.get()];
 	}
 
 	@Override
-	public void run() {		
+	public void run() {
 		String responseLine;
 
-		while(!close.get()) {
+		while (!close.get()) {
 			// Initialization section:
 			// Try to open a socket on a given host and port
 			// Try to open input and output streams
@@ -95,7 +103,7 @@ public class TcpReceive  implements Runnable{
 				changeConnectionState(ConnectionState.Connected);
 			} catch (Exception e) {
 				System.err.println("Don't know about host " + host);
-				
+
 				try {
 
 					if (clientSocket.isConnected()) {
@@ -109,18 +117,26 @@ public class TcpReceive  implements Runnable{
 				} catch (Exception e1) {
 				}
 				continue;
-			} 
+			}
 
-			// Keep on reading from the socket till we receive the "Bye" from the server,
+			// Keep on reading from the socket till we receive the "Bye" from
+			// the server,
 			// once we received that then we want to break.
-			try{ 
+			try {
 				while ((responseLine = is.readUTF()) != null) {
-					dataReceived(responseLine);
+					responseLine = responseLine.replaceAll(KEEP_ALIVE, "");
+					
+					if (responseLine.equals(STOP_CONNECTION)) {
+						break;
+					}
+					
+					if (!responseLine.isEmpty()) {
+						dataReceived(responseLine);
+					}
 				}
 			} catch (IOException e) {
 				System.err.println("IOException:  " + e);
-			} 
-			finally {
+			} finally {
 
 				// Clean up:
 				// close the input stream
@@ -129,15 +145,15 @@ public class TcpReceive  implements Runnable{
 					is.close();
 				} catch (IOException e) {
 					e.printStackTrace();
-				} 
+				}
 
 				try {
 					if (clientSocket.isConnected()) {
-						clientSocket.close(); 
+						clientSocket.close();
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
-				}  
+				}
 			}
 		}
 	}
