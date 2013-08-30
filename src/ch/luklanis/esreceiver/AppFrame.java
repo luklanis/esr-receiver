287844302205537eb77afdf53710df2d53a26ba8
Lukas Landis <landis.lukas@gmail.com>
2013-08-30 13:39:29 +0200

    using websocket library

--- src/ch/luklanis/esreceiver/AppFrame.java
+++ src/ch/luklanis/esreceiver/AppFrame.java
@@ -63,7 +63,7 @@ public class AppFrame extends JFrame implements ClipboardOwner {
 	 * 
 	 */
 	private static final long serialVersionUID = -1668274381664960966L;
-	private static final String CURRENT_VERSION = "0.6.2";
+	private static final String CURRENT_VERSION = "0.6.3";
 	private static final String propertiesFile = System.getProperty("user.dir")
 			+ "/ESRReceiver.properties";
 
--- src/ch/luklanis/esreceiver/TcpReceive.java
+++ src/ch/luklanis/esreceiver/TcpReceive.java
@@ -1,13 +1,21 @@
 package ch.luklanis.esreceiver;
 
+import java.io.Console;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import com.sun.jndi.toolkit.url.Uri;
 
 import ch.luklanis.esreceiver.connectionstate.ConnectionState;
 import ch.luklanis.esreceiver.connectionstate.ConnectionStateChangedEvent;
@@ -15,29 +23,21 @@ import ch.luklanis.esreceiver.connectionstate.OnConnectionStateChangeListener;
 import ch.luklanis.esreceiver.datareceived.DataReceivedEvent;
 import ch.luklanis.esreceiver.datareceived.OnDataReceivedListener;
 
-public class TcpReceive implements Runnable {
+public class TcpReceive {
 
 	// Declaration section
 	// clientClient: the client socket
 	// os: the output stream
 	// is: the input stream
-	private static final AtomicBoolean close = new AtomicBoolean(false);
 	private static final AtomicInteger currentState = new AtomicInteger(
 			ConnectionState.Disconnected.ordinal());
-	
-	private static final String KEEP_ALIVE = "KA";
-	private static final String STOP_CONNECTION = "STOP";
-	private static final String START_SERVER = "START";
 
-	private static final String ACK = "ACK";
-
-	static Socket clientSocket = null;
+	private WebSocketClient webSocketClient;
 
 	private OnConnectionStateChangeListener onConnectionStateChangeListener;
 	private OnDataReceivedListener onDataReceivedListener;
 	private String host;
 	private int port;
-	private static Thread mReceiveDataThread;
 
 	public void setOnConnectionStateChangeListener(
 			OnConnectionStateChangeListener listener) {
@@ -49,44 +49,49 @@ public class TcpReceive implements Runnable {
 	}
 
 	public void close() {
-		close.set(true);
-
-		if (mReceiveDataThread != null) {
-			if (clientSocket != null) {
-				try {
-					clientSocket.close();
-				} catch (IOException e) {
-				}
-				clientSocket = null;
-			}
-			
-			try {
-				mReceiveDataThread.join(500);
-			} catch (InterruptedException e) {
-			}
-			
-			if (mReceiveDataThread.isAlive()) {
-				mReceiveDataThread.interrupt();
-			}
-
-			mReceiveDataThread = null;
-		}
+		webSocketClient.close();
 
 		changeConnectionState(ConnectionState.Disconnected);
 	}
 
 	public void connect(String host, int port) {
-
-		close.set(false);
 		this.host = host;
 		this.port = port;
 
 		changeConnectionState(ConnectionState.Connecting);
+		
+		String uriString = String.format("ws://%s:%s", this.host, this.port);
 
-		if (mReceiveDataThread == null) {
-			mReceiveDataThread = new Thread(this);
-			mReceiveDataThread.setName("receiveDataThread");
-			mReceiveDataThread.start();
+		try {
+			webSocketClient = new WebSocketClient(new URI(uriString)) {
+				
+				@Override
+				public void onOpen(ServerHandshake arg0) {
+					changeConnectionState(ConnectionState.Connected);
+				}
+				
+				@Override
+				public void onMessage(String arg0) {
+					dataReceived(arg0);
+				}
+				
+				@Override
+				public void onError(Exception arg0) {
+					System.out.println(arg0.getMessage());
+				}
+				
+				@Override
+				public void onClose(int arg0, String arg1, boolean arg2) {
+					changeConnectionState(ConnectionState.Connecting);
+					
+					TcpReceive self = TcpReceive.this;
+					self.connect(self.host, self.port);
+				}
+			};
+			
+			webSocketClient.connect();
+		} catch (URISyntaxException e) {
+			System.out.println(uriString + " is not a valid WebSocket URI\n");
 		}
 	}
 
@@ -108,105 +113,5 @@ public class TcpReceive implements Runnable {
 
 	public ConnectionState getCurrentState() {
 		return ConnectionState.values()[currentState.get()];
-	}
-
-	@Override
-	public void run() {
-		String responseLine = "";
-		DataInputStream is = null;
-		DataOutputStream os = null;
-		
-
-		while (!close.get()) {
-			// Initialization section:
-			// Try to open a socket on a given host and port
-			// Try to open input and output streams
-			try {
-				changeConnectionState(ConnectionState.Connecting);
-				clientSocket = new Socket(host, port);
-				is = new DataInputStream(clientSocket.getInputStream());
-				//clientSocket.setSoTimeout(5000);
-				changeConnectionState(ConnectionState.Connected);
-			} catch (Exception e) {				
-				try {
-					if (clientSocket.isConnected()) {
-						clientSocket.close();
-					}
-				} catch (Exception e1) {
-				}
-
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e1) {
-				}
-				continue;
-			}
-
-			// Keep on reading from the socket till we receive the "Bye" from
-			// the server,
-			// once we received that then we want to break.
-			try {
-				if ((responseLine = is.readUTF()) != null) {
-					responseLine = responseLine
-							.replaceAll(KEEP_ALIVE, "")
-							.replaceAll(START_SERVER, "");
-
-					if (!responseLine.isEmpty()) {
-
-						os = new DataOutputStream(
-								clientSocket.getOutputStream());
-						os.writeUTF(ACK);
-						os.flush();
-					}
-					if (!responseLine.equals(STOP_CONNECTION)
-							&& !responseLine.isEmpty()) {
-						dataReceived(responseLine);
-					}
-				}
-			} catch (SocketTimeoutException e) {
-			} catch (SocketException e) {
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-
-				// Clean up:
-				// close the input stream
-				// close the socket
-				if (is != null) {
-					try {
-						is.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					is = null;
-				}
-
-				if (os != null) {
-					try {
-						os.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					os = null;
-				}
-
-				if (clientSocket != null) {
-					try {
-						clientSocket.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					clientSocket = null;
-				}
-
-				if (responseLine.equals(STOP_CONNECTION)) {
-					changeConnectionState(ConnectionState.Connecting);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-					}
-				}
-			}
-		}
 	}
 }
